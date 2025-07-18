d13c61f57a9d9933b8e49315065987ebfbdc29b5
aliM-dev-sys <alimohamed6255@gmail.com>
2025-07-18 16:42:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const trends = require('google-trends-api');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.get('/api/trends', async (req, res) => {
   const keyword = req.query.keyword || 'AI';
